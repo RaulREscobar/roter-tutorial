e48f046fa7bcf3a749176e53c8ae59ce841404d2
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2022-05-05 11:20:16 -0300

    aplicación react router dom v6 siguiendo el tutorial de la documentacion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Bookkeeper! / Contador!</h1>
+        <nav style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem"
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Link to="/invoices">Invoices/Facturas</Link> |{" "}
+          <Link to="/expenses">Expenses/Gastos</Link>
+        </nav>
+        <Outlet />
     </div>
+    
   );
 }
 
