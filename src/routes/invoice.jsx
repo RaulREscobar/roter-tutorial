6e82c7f61cc508d822e3955cb17f5934d030de5f
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2022-05-07 21:22:50 -0300

    se agrega mas info ala seccion de facturas

--- src/routes/invoice.jsx
+++ src/routes/invoice.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { getInvoice } from "../data"
 
 export default function Invoice() {
     let params = useParams();
+    let invoice = getInvoice(parseInt(params.invoiceId, 10));
   return (
-    <h2>Invoice: {params.invoiceId}</h2>
-  )
+    <main style={{ padding: "1rem"}}>
+      <h2>Total Due / Total vencido: {invoice.amount}</h2>
+      <p>
+        {invoice.name}: {invoice.number}
+      </p>
+      <p>Due Date / fecha de vencimiento: {invoice.due}</p>
+    </main>
+  );
 }
