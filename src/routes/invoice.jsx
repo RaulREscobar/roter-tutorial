f50bf97ea56c83df925410f3325939a499a062c0
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2022-05-07 22:21:45 -0300

    se agrega un boton de borrar cada factura desde la url y se termina tutorial

--- src/routes/invoice.jsx
+++ src/routes/invoice.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import { getInvoice } from "../data"
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { getInvoice, deleteInvoice } from "../data"
 
 export default function Invoice() {
+    let navigate = useNavigate()
+    let location = useLocation()
     let params = useParams();
     let invoice = getInvoice(parseInt(params.invoiceId, 10));
   return (
@@ -12,6 +14,16 @@ export default function Invoice() {
         {invoice.name}: {invoice.number}
       </p>
       <p>Due Date / fecha de vencimiento: {invoice.due}</p>
+      <p>
+        <button 
+          onClick={()=> {
+            deleteInvoice(invoice.number);
+            navigate("/invoices" + location.search);
+          }}
+        >
+          Delete / Borrar
+        </button>
+      </p>
     </main>
   );
 }
